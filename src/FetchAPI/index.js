d451e6a7f1aa707bec305845f1b334c7e244b337
saravilarinho <ssvilarinho@ua.pt>
2021-02-28 15:07:50 +0000

    posts nas categorias

--- src/FetchAPI/index.js
+++ src/FetchAPI/index.js
@@ -7,9 +7,12 @@ const fetchFromAPI = (resourceType) =>
 
 export const fetchCategory = () => fetchFromAPI('categories');
 
-
 export const fetchPostsCategory = (id) =>
     fetch(`http://localhost:3001/categories/${id}`)
         .then(response => response.json())
 
+/*export const fetchICategory = (id) =>
+    fetch(`http://localhost:3001/categories/${id}`)
+        .then(response => response.json())*/
 
+export const fetchICategory = (id) => fetchFromAPI('categories');
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -12,7 +12,7 @@ import imgChinese from "../../images/subcategories/restaurants_chinese.png";
 import {AiOutlineLike} from "react-icons/ai";
 import Navbar from "../../components/Navbar";
 import HeaderBack from "../../components/HeaderBack";
-import {fetchPostsCategory} from "../../FetchAPI";
+import {fetchICategory, fetchPostsCategory} from "../../FetchAPI";
 import {useParams} from "react-router-dom";
 
 const Title = styled.h4`
@@ -20,8 +20,7 @@ const Title = styled.h4`
   font-weight: 800;
   color: #002337;`
 const BtnFollow = styled(Link)`
-  color: #F7A70A;
-  border: solid 1px #F7A70A;
+        border: 1px solid;
   border-radius: 2px;
   text-align: center;
   height: 30px;
@@ -46,7 +45,6 @@ const BtnSearch = styled(Link)`
 const Tips = styled.div`
   `
 const TopTips = styled.div`
-  background-color: #F7A70A;
   padding-bottom: 5px;`
 const TopTip = styled.div`
   background-color: white;
@@ -91,8 +89,7 @@ const Sub = styled.div`
   height: 70px;
   width: 70px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);`
-const InfoAdd = styled.div`
-`
+const InfoAdd = styled.div``
 const Label = styled.p`
   font-size: 16px;
   font-weight: 700;
@@ -112,11 +109,17 @@ export default () => {
 
     const [posts, setPosts] = useState([]);
     const [isLoading, setisLoading] = useState(false)
+    const [nome, setNome] = useState([])
 
     const {id} = useParams();
 
     useEffect(() => {
         setisLoading(true)
+        fetchICategory(id)
+            .then(nome => {
+             setNome(nome);
+            })
+
         fetchPostsCategory(id)
             .then(posts => {
                     setPosts(posts);
@@ -132,15 +135,26 @@ export default () => {
     return (
         <div>
             <HeaderBack/>
-            <div className="d-flex">
-                <Title className="mt-2 mb-2 mr-5">
-                    RESTAURANTS
-                </Title>
-                <BtnFollow className="align-self-center text-decoration-none">Follow</BtnFollow>
-            </div>
+
+                    {nome.map(
+                        (Nome)=> {
+                            if (Nome.id === id){
+                               return(
+                                   <div className="d-flex">
+                                       <Title className="mt-2 mb-2 mr-5 text-uppercase">
+                                        {Nome.name}
+                                       </Title>
+                                       <BtnFollow className="align-self-center text-decoration-none" style={{color: Nome.color, borderColor: Nome.color, }}>Follow</BtnFollow>
+                                   </div>
+                               )
+                            }
+                        }
+                        )}
+
+
 
             <div className="d-flex text-center mt-4">
-                <BtnCreate className="col-9 mr-4" to={"/createpost"}>
+                <BtnCreate className="col-9 mr-4" to={"/createpost/"+ id}>
                     <FaEdit className="mr-2"/> Create Post
                 </BtnCreate>
                 <BtnSearch className="col-2" to={"/searchresults"}>
@@ -184,163 +198,69 @@ export default () => {
 
 
             <Tips>
-                <TopTips className="mb-3">
-                    <TitleTopTips className="mt-0 pt-2 pl-3">TOP-TIPS</TitleTopTips>
+                {
+                    nome.map(
+                        (Nome)=> {
+                            if (Nome.id === id){
 
-                    <Accordion className="align-self-center col-12 pl-0 pr-0">
+                                return(
+                                    <TopTips className="mb-3"  style={{backgroundColor: Nome.color}}>
 
-                        <TopTip className="d-flex row mb-3">
-                            <div className="col-3 pl-0 mt-2">
-                                <img className="rounded-circle" src={imgUser} alt="profile"/>
-                            </div>
-                            <div className="col-7 pl-0 mt-2">
-                                <Text>
-                                    Best restaurant in Aveiro
-                                </Text>
-                                <Name>
-                                    okaaaa
-                                </Name>
-                            </div>
+                                        <TitleTopTips className="mt-0 pt-2 pl-3">TOP-TIPS</TitleTopTips>
 
-                            <Accordion.Toggle eventKey="0" style={{border: "transparent", backgroundColor: "white"}}
-                                              className="col-2">
-                                <FaChevronDown/>
-                            </Accordion.Toggle>
+                                        {posts.map(
+                                            (Post)=> {
+                                                return(
+                                                    <Accordion className="align-self-center col-12 pl-0 pr-0">
 
-                            <Accordion.Collapse eventKey="0">
-                                <div>
-                                    <h6 className="mt-3">This was the best restaurant I've been, it was a wonderful
-                                        experience, the food is wonderful, the saft is very friendly and very
-                                        attentive. </h6>
-                                    <InfoAdd className="d-flex">
-                                        <Date className="col-8 pl-0 ml-0 mb-0">
-                                            04-01-2020 at 23:12
-                                        </Date>
-                                        <Likes className="ml-4 col-md-4 mb-0">
-                                            <p>2376</p>
-                                            <BotaoLike>
-                                                <AiOutlineLike className={"w-100"}/>
-                                            </BotaoLike>
+                                                        <TopTip className="d-flex row mb-3">
+                                                            <div className="col-3 pl-0 mt-2">
+                                                                <img className="rounded-circle" src={imgUser} alt="profile"/>
+                                                            </div>
+                                                            <div className="col-7 pl-0 mt-2">
+                                                                <Text>
+                                                                    {Post.title}
+                                                                </Text>
+                                                                <Name>
+                                                                    Anne-Marie
+                                                                </Name>
+                                                            </div>
 
-                                        </Likes>
-                                    </InfoAdd>
-                                </div>
-                            </Accordion.Collapse>
-                        </TopTip>
-                    </Accordion>
+                                                            <Accordion.Toggle eventKey="0" style={{border: "transparent", backgroundColor: "white"}}
+                                                                              className="col-2">
+                                                                <FaChevronDown/>
+                                                            </Accordion.Toggle>
 
+                                                            <Accordion.Collapse eventKey="0">
+                                                                <div>
+                                                                    <h6 className="mt-3">{Post.text} </h6>
+                                                                    <InfoAdd className="d-flex">
+                                                                        <Date className="col-8 pl-0 ml-0 mb-0">
+                                                                            04-01-2020 at 23:12
+                                                                        </Date>
+                                                                        <Likes className="ml-4 col-md-4 mb-0">
+                                                                            <p>{Post.likes}</p>
+                                                                            <BotaoLike>
+                                                                                <AiOutlineLike className={"w-100"}/>
+                                                                            </BotaoLike>
 
+                                                                        </Likes>
+                                                                    </InfoAdd>
+                                                                </div>
+                                                            </Accordion.Collapse>
+                                                        </TopTip>
+                                                    </Accordion>
+                                                )
+                                            }
+                                        )
+                                        }
+                                    </TopTips>
+                                )
 
-                    {posts.map(
-                        (Post)=> {
-                            return(
-                                <Accordion className="align-self-center col-12 pl-0 pr-0">
-
-                                    <TopTip className="d-flex row mb-3">
-                                        <div className="col-3 pl-0 mt-2">
-                                            <img className="rounded-circle" src={imgUser} alt="profile"/>
-                                        </div>
-                                        <div className="col-7 pl-0 mt-2">
-                                            <Text>
-                                                {Post.title}
-                                            </Text>
-                                            <Name>
-                                                Anne-Marie
-                                            </Name>
-                                        </div>
-
-                                        <Accordion.Toggle eventKey="0" style={{border: "transparent", backgroundColor: "white"}}
-                                                          className="col-2">
-                                            <FaChevronDown/>
-                                        </Accordion.Toggle>
-
-                                        <Accordion.Collapse eventKey="0">
-                                            <div>
-                                                <h6 className="mt-3">This was the best restaurant I've been, it was a wonderful
-                                                    experience, the food is wonderful, the saft is very friendly and very
-                                                    attentive. </h6>
-                                                <InfoAdd className="d-flex">
-                                                    <Date className="col-8 pl-0 ml-0 mb-0">
-                                                        04-01-2020 at 23:12
-                                                    </Date>
-                                                    <Likes className="ml-4 col-md-4 mb-0">
-                                                        <p>2376</p>
-                                                        <BotaoLike>
-                                                            <AiOutlineLike className={"w-100"}/>
-                                                        </BotaoLike>
-
-                                                    </Likes>
-                                                </InfoAdd>
-                                            </div>
-                                        </Accordion.Collapse>
-                                    </TopTip>
-                                </Accordion>
-                            )
+                            }
                         }
                     )
-                    }
-
-
-                    <Accordion className="align-self-center col-12 pl-0 pr-0">
-
-                        <TopTip className="d-flex row mb-3">
-                            <div className="col-3 pl-0 mt-2">
-                                <img className="rounded-circle" src={imgUser} alt="profile"/>
-                            </div>
-                            <div className="col-7 pl-0 mt-2">
-                                <Text>
-                                    Best restaurant in Aveiro
-                                </Text>
-                                <Name>
-                                    Anne-Marie
-                                </Name>
-                            </div>
-
-                            <Accordion.Toggle eventKey="0" style={{border: "transparent", backgroundColor: "white"}}
-                                              className="col-2">
-                                <FaChevronDown/>
-                            </Accordion.Toggle>
-
-                            <Accordion.Collapse eventKey="0">
-                                <div>
-                                    <h6 className="mt-3">This was the best restaurant I've been, it was a wonderful
-                                        experience, the food is wonderful, the saft is very friendly and very
-                                        attentive. </h6>
-                                    <InfoAdd className="d-flex">
-                                        <Date className="col-8 pl-0 ml-0 mb-0">
-                                            04-01-2020 at 23:12
-                                        </Date>
-                                        <Likes className="ml-4 col-md-4 mb-0">
-                                            <p>2376</p>
-                                            <BotaoLike>
-                                                <AiOutlineLike className={"w-100"}/>
-                                            </BotaoLike>
-
-                                        </Likes>
-                                    </InfoAdd>
-                                </div>
-                            </Accordion.Collapse>
-                        </TopTip>
-                    </Accordion>
-
-                    <TopTip className="d-flex row mb-3">
-                        <div className="col-3 pl-0">
-                            <img className="rounded-circle" src={imgUser} alt="profile"/>
-                        </div>
-                        <div className="col-7 pl-0">
-                            <Text>
-                                Best restaurant in Aveiro
-                            </Text>
-                            <Name>
-                                Anne-Marie
-                            </Name>
-                        </div>
-                        <div className="align-self-center col-2 pr-0">
-                            <FaChevronDown/>
-                        </div>
-                    </TopTip>
-                </TopTips>
-
+                }
 
                 <Accordion className="align-self-center col-12 pl-0 pr-0">
 
