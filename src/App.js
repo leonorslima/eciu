3fb4812e644bc5e9e8e63592bd4d996bd57f086c
saravilarinho <ssvilarinho@ua.pt>
2021-03-02 17:18:32 +0000

    feedback sign up

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ import CreatePost from "./pages/CreatePost/index";
 import SearchResults from "./pages/SearchResults/index";
 import Home from "./pages/Home/index";
 import PrivateChat from "./pages/PrivateChat/Index";
+import firebase from "firebase/app";
+import "firebase/auth";
+import FirebaseConfig from './scripts/FirebaseConfig';
+import {FirebaseAuthProvider} from "@react-firebase/auth";
+
 function App() {
 
     /*useEffect(() => {
@@ -38,29 +43,33 @@ function App() {
 */
 
     return (
-    <div className="mr-3 ml-3">
-        <Router>
-            <Switch>
-                <Route exact path={"/"} component={Feed}/>
-                <Route exact path={"/feed"} component={Feed} />
-                <Route exact path={"/categories"} component={Categories} />
-                <Route exact path={"/categories/:id"} component={Post} />
-                <Route exact path={"/community"} component={Community} />
-                <Route exact path={"/chat"} component={Chat}/>
-                <Route exact path={"/myprofile"} component={MyProfile}/>
-                <Route exact path={"/editprofile"} component={EditProfile}/>
-                <Route exact path={"/profile"} component={Profile}/>
-                <Route exact path={"/profile/:id"} component={Profile}/>
-                <Route exact path={"/signup"} component={SignUp}/>
-                <Route exact path={"/signin"} component={SignIn}/>
+        <FirebaseAuthProvider firebase={firebase} {...FirebaseConfig}>
+            {
+                <div className="mr-3 ml-3">
+                    <Router>
+                        <Switch>
+                            <Route exact path={"/"} component={Feed}/>
+                            <Route exact path={"/feed"} component={Feed} />
+                            <Route exact path={"/categories"} component={Categories} />
+                            <Route exact path={"/categories/:id"} component={Post} />
+                            <Route exact path={"/community"} component={Community} />
+                            <Route exact path={"/chat"} component={Chat}/>
+                            <Route exact path={"/myprofile"} component={MyProfile}/>
+                            <Route exact path={"/editprofile"} component={EditProfile}/>
+                            <Route exact path={"/profile"} component={Profile}/>
+                            <Route exact path={"/profile/:id"} component={Profile}/>
+                            <Route exact path={"/signup"} component={SignUp}/>
+                            <Route exact path={"/signin"} component={SignIn}/>
+                            <Route exact path={"/createpost/:id"} component={CreatePost}/>
+                            <Route exact path={"/searchresults"} component={SearchResults}/>
+                            <Route exact path={"/home"} component={Home}/>
+                            <Route exact path={"/privatechat"} component={PrivateChat}/>
+                        </Switch>
+                    </Router>
+                </div>
+            }
+        </FirebaseAuthProvider>
 
-                <Route exact path={"/createpost/:id"} component={CreatePost}/>
-                <Route exact path={"/searchresults"} component={SearchResults}/>
-                <Route exact path={"/home"} component={Home}/>
-                <Route exact path={"/privatechat"} component={PrivateChat}/>
-            </Switch>
-        </Router>
-    </div>
   );
 }
 
